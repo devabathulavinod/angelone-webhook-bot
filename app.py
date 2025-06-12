ba75eec0c0ed76162b78c7e9a744829f66c7a3fd
devabathulavinod <68983231+devabathulavinod@users.noreply.github.com>
2025-06-12 17:24:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,7 @@ def home():
     return "Angel One Webhook is live!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
