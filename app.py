5aeb7ec2d4a3f3cc68dee81791540705eaf9a69f
devabathulavinod <68983231+devabathulavinod@users.noreply.github.com>
2025-06-17 11:25:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def webhook():
         print("❌ Unsupported content type")
         return "Unsupported Media Type", 415
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
+
 
 
